d3ee5e1b44b65f8f906718fc0f7e9eb97a5a1e26
sleepycat-glitch <asleepycat15@gmail.com>
2025-12-09 15:30:56 +0000

    loops_2025

--- 1_loops.py
+++ 1_loops.py
@@ -7,6 +7,7 @@ fruits = ["apple", "banana", "cherry", "date"]
 print(fruits[0])
 print(fruits[1])
 print(fruits[2])
+#abstracted version of previous code
 for fruit in fruits:
     print(fruit)
 # i just worked with loops
@@ -15,9 +16,25 @@ for fruit in fruits:
 subjects = ["Math", "Science", "History", "Art"]
 for subject in subjects:
     print(subject)
+
+# print out each subject but stops when reach "History"
+for subject in subjects:
+    if subject=="History":
+        break
+    else:
+        print(subject)
+
+# prints out each subject but skips over "Science"
+for subject in subjects:
+    if subject=="Science":
+        continue
+    else:
+        print(subject)
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
+for i in range(len(subjects)):
+    print(f"Subject {i}: {subjects[i]}")
 
 
 # Given:
@@ -25,3 +42,10 @@ numbers = [5, 10, 15, 20]
 
 # Challenge:
 # Use a for loop to add all the numbers and print the total.
+# set base variable
+# += adds current value to previous value
+total = 0
+for number in numbers:
+    total+=number
+print(total)
+    
--- 2_while_loops.py
+++ 2_while_loops.py
@@ -1,6 +1,56 @@
-# Given:
-colors = ["red", "blue", "green", "yellow", "purple"]
+# notes:
+# name = input("Enter your name: ")
+# executes code while true, while false, exits loop
+# if no brake in while loop, will continue forever
+# while name == "":
+#     print("You did not enter your name.")
+#     name = input("Enter your name: ")
+# print(f"Hello {name}.")
 
-# Challenge:
-# Use a while loop to print each color UNTIL you find "yellow".
-# Do NOT print "yellow" — stop before it.
+
+# age = int(input("Enter your age: "))
+
+# while age<0:
+#     print("Age can't be negative.")
+#     age = int(input("Enter your age: "))
+# print(f"You are {age} years old.")
+
+# foods = [ ]
+# # if empty list is inside while loop, then it will erase the list every time it runs
+# food = input("Enter a food you like (press q to quit): ")
+# while not food == "q":
+#     foods.append(food)
+#     print(f"You like {food}.")
+#     food = input("Enter a food you like (press q to quit): ")
+# print(f"You like the following foods: {foods}")
+# print("Bye.")
+
+
+# num = int(input("Enter a number between 1-10: "))
+# while num <1 or num>10:
+#     print(f"{num} is not valid.")
+#     num = int(input("Enter a number between 1-10: "))
+# print(f"Your number is {num}.")
+
+    
+# # Given:
+# colors = ["red", "blue", "green", "yellow", "purple"]
+
+# # Challenge:
+# # Use a while loop to print each color UNTIL you find "yellow".
+# # Do NOT print "yellow" — stop before it.
+
+# index = 0
+# while index<len(colors):
+#     if colors[index] == "yellow":
+#         break
+#     print(colors[index])
+#     index+= 1 # increments index to avoid infinite loop
+
+# # for loop version of previous code 
+# for color in colors:
+#     if color =="yellow":
+#         break
+#     else:
+#         print(color)
+
